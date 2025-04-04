47691a039542307e88b236ae9f53057f115a4079
Zeng Bo <zengbo1228@gmail.com>
2025-04-05 00:04:47 +0800

    change all rules to warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@ import eslint from "eslint";
 import nextPlugin from "@next/eslint-plugin-next";
 import typescriptEslint from "@typescript-eslint/eslint-plugin";
 import tsParser from "@typescript-eslint/parser";
+import reactPlugin from "eslint-plugin-react";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -23,23 +24,33 @@ const eslintConfig = [
     plugins: {
       '@next/next': nextPlugin,
       '@typescript-eslint': typescriptEslint,
+      'react': reactPlugin
     },
     languageOptions: {
       parser: tsParser,
       parserOptions: {
         project: './tsconfig.json',
+        ecmaFeatures: {
+          jsx: true
+        }
       }
     },
     rules: {
-      // Convert all errors to warnings
+      // Convert all rules to warnings
       'no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'no-debugger': 'warn',
+      
+      // TypeScript rules
       '@typescript-eslint/no-unused-vars': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/explicit-function-return-type': 'warn',
       '@typescript-eslint/no-non-null-assertion': 'warn',
       '@typescript-eslint/no-unnecessary-type-assertion': 'warn',
+      '@typescript-eslint/no-empty-interface': 'warn',
+      '@typescript-eslint/no-inferrable-types': 'warn',
       
-      // Next.js specific rules
+      // Next.js rules
       '@next/next/no-img-element': 'warn',
       '@next/next/no-html-link-for-pages': 'warn',
       '@next/next/no-sync-scripts': 'warn',
@@ -48,13 +59,16 @@ const eslintConfig = [
       'react/prop-types': 'warn',
       'react/no-unescaped-entities': 'warn',
       'react/react-in-jsx-scope': 'warn',
+      'react/no-direct-mutation-state': 'warn',
+      'react/no-deprecated': 'warn',
       
-      // General warnings
-      'no-console': 'warn',
-      'no-debugger': 'warn',
+      // Accessibility rules
+      'jsx-a11y/alt-text': 'warn',
+      'jsx-a11y/aria-role': 'warn',
       
-      // Disable specific errors completely
+      // Completely disable some strict checks
       '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-var-requires': 'off'
     }
   },
   
